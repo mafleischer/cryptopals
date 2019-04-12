4b933fd9452e5ca34fac2ffb06c253d74a5f3aa6
ullu <ullu@ullu.de>
2019-04-12 20:53:39 +0200

    did challenge 16, with cheating; automate at some point

--- crypto_algos/attack/blockcipher/cbc.py
+++ crypto_algos/attack/blockcipher/cbc.py
@@ -2,7 +2,10 @@
 
 
 def cipherWFlippedBytesGenerator(bstr_cipher, *indexes):
-    """ used for breaking cbc; flip the bytes in the positions given in indexes list """
-    # nonsense
-    index_to_bytes = tuple( ( (i, b) for i, b in zip(indexes, bstr_cipher[i]) ) )
-    for t in index_to_bytes:
+    """ used for breaking cbc; flip the bytes in the positions given in indexes list;
+    intended but not used so far
+    """
+    index_to_bytes = tuple( ( (i, bstr_cipher[i]) for i in indexes ) )
+    for tup in index_to_bytes:
+    	new_cipher = bstr_cipher
+    	yield new_cipher
--- set2/16-cbc-bitflipping.py
+++ set2/16-cbc-bitflipping.py
@@ -4,13 +4,16 @@ from crypto_algos import aes
 from crypto_algos.helpers import andBytestrings
 from crypto_algos.challenge_specific import setupCBCSecretMaker
 from crypto_algos.attack.blockcipher.misc import findCipherBlockJumpLen
+from crypto_algos.attack.blockcipher import cbc
 import re
 
 
 def _checkAdminPerm(bstr_cipher, bstr_key, bstr_IV):
     bstr_clear = aes.aesDecrypt(
-        bstr_cipher, bstr_key, 128, mode='cbc', bstr_IV=bstr_IV)
-    clear = bstr_clear.decode()
+       	bstr_cipher, bstr_key, 128, mode='cbc', bstr_IV=bstr_IV)
+    print(bstr_clear[48:])
+    # clear = bstr_clear.decode()
+    clear = repr(bstr_clear)
     result = re.search(';admin=true;', clear)
     if result:
     	return True
@@ -27,19 +30,37 @@ secret_fn = setupCBCSecretMaker(bstr_key, bstr_IV)
 padding_len = findCipherBlockJumpLen(secret_fn)
 
 # change second S to semicolon and E to equal sign through byte flip
-permission_string = b'x' * padding_len + b'12345S781E345678XXXXXSadminEtrue'
+#permission_string = b'x' * padding_len + b'01234S78901E3456XXXXX;adminEtrue'
+permission_string = b'01234S78901E3456XXXXXSadminEtrue'
 
-cipher = secret_fn(b'aaaa;admin=true')
 cipher = secret_fn(permission_string)
-print(cipher)
-
-#cipher = aes.aesEncrypt(b'xxxxvvvvbbbbggggAAAAAAAAAAAAAAAA', bstr_key, 128, mode='cbc', bstr_IV=b'1234567887654321')
 # print(cipher)
 
-#cipher = andBytestrings(cipher, b'')
-# cipher = b'#\xca\xc4\xc3\xea\xaf\x88@\xde\x17G\xa5\x8d\x17\x9a\xb5\xaa\xd2\xea\xe8GJ\x93\x152I\xa0|1\xceo0'
-# cipher =
-# b'#\xca\xc8\xc3\xea\xaf\x88@\xde\x17G\xa5\x8d\x17\x9a\xb5\xaa\xd2\xea\xe8GJ\x93\x152I\xa0|1\xceo0'
+# flipped_cipher_iter = cbc.cipherWFlippedBytesGenerator(cipher, 1, 3)
+# for c in flipped_cipher_iter:
+#	print(c)
+
+# cipher = aes.aesEncrypt(b'xxxxvvvvbbbbggggAAAAAAAAAAAAAAAA', bstr_key, 128, mode='cbc', bstr_IV=b'1234567887654321')
+
+print(hex(cipher[padding_len + 32 + 5]))
+print(hex(cipher[padding_len + 32 + 11]))
+
+# for b in range(256):
+# 	tmp = bytearray(cipher)
+# 	#tmp[padding_len + 32 + 5] = b
+# 	tmp[32 + 11] = b
+# 	cipher = bytes(tmp)
+# 	if _checkAdminPerm(cipher, bstr_key, bstr_IV):
+# 		print('yes. byte: {0}'.format(b))
+# 		break
+
+b1 = 0xd8
+b2 = 0xa8
+tmp = bytearray(cipher)
+tmp[32 + 5] = b1
+tmp[32 + 11] = b2
+cipher = bytes(tmp)
+
 
 if _checkAdminPerm(cipher, bstr_key, bstr_IV):
 	print('yes')

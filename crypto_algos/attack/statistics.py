953940ab3dddec8aabaa749f73720e609a50e161
ullu <ullu@ullu.de>
2019-06-28 20:06:54 +0200

    Add statistics.py in attack module
    Make some proper statistic methods in own module
    for some challenges, take them from the first
    challenges and reqrite those maybe.

---
+++ crypto_algos/attack/statistics.py
@@ -0,0 +1,51 @@
+import struct
+
+english_letters = (,)
+english_bigrams = (,)
+
+
+def hammingDistance(bstr1: bytes, bstr2: bytes) -> int:
+    """
+    Compute Hamming Distance between two strings
+
+    :param bstr1: byte string 1
+    :param bstr2: byte string 2
+    :return: int
+    """
+    if len(bstr1) != len(bstr2):
+        print("xor not equal len!")
+        exit(1)
+    diff = b''
+    b1 = bstr1
+    b2 = bstr2
+    diff = xor(b1, b2)
+    distance = 0
+    for b in diff:
+        distance += bin(b).count("1")
+    return(distance)
+
+
+def bytesFrequency(bstr: bytes, len: int) -> dict:
+    """
+    Count the occurences of each distinct group of bytes of length len in bstr
+
+    :param bstr: byte string
+    :return: a dictionary mapping bytes to their frequency in bstr (key is frequency)
+    """
+    pass
+
+
+def mapFreqsToLang(dict_bytes_to_freqs: dict, language_tuple: tuple) -> dict:
+    """
+    Map dictionary (obtained from bytesFrequency) items, i.e. the bytes obtained from
+    a byte string, to bytes in the language tuple that have the same frequency in that language.
+    The frequency is the key in the dict and the index position in the tuple.
+
+    :param dict_bytes_to_freqs: dict mapping {<occurence in bytestr>: b'foo'}
+    :param language_tuple: a tuple with characters, bigrams or trigrams etc.
+    They have the same length as the value bytes in the dict.
+    Index of a char or group of bytes corresponds to its frequency in the
+    language
+    :return: dict mapping {b'foo': b'bar'}
+    """
+    pass

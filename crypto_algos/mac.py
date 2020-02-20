31ee47539c76b4feb24523f1c7097e63432d17bb
ullu <ullu@ullu.de>
2020-02-20 22:18:38 +0100

    Remove most function from mac.py again
    Don't know what they were

--- crypto_algos/mac.py
+++ crypto_algos/mac.py
@@ -1,4 +1,5 @@
-from crypto_algos.submodules.sha1 import sha1
+import types
+from crypto_algos.submodules.sha1.sha1 import sha1
 from crypto_algos import aes
 
 
@@ -10,22 +11,3 @@ def sha1MAC(bstr_key: bytes, bstr_clear: bytes) -> bytes:
     :return: sha1 keyed MAC tag
     """
     return sha1(bstr_key + bstr_clear).encode('ascii')
-
-
-def sha1MACTagCipher(bstr_cipher: bytes, bstr_clear: bytes, bstr_key: bytes) -> bytes:
-    """
-
-    :param bstr_cipher:
-    :param bstr_clear:
-    :param bstr_key:
-    :return:
-    """
-    tag = sha1MAC(bstr_key, bstr_clear)
-    return bstr_cipher + tag
-
-
-def sha1MACAuthMsg(bstr_cipher_tag: bytes, bstr_key: bytes, bstr_clear: bytes) -> bool:
-    tag = bstr_cipher_tag[-40:]
-    if sha1MAC(bstr_key, bstr_clear).encode('ascii') == tag:
-        return True
-    return False
--- set4/28-implement-sha1-mac.py
+++ set4/28-implement-sha1-mac.py
@@ -1,5 +1,30 @@
 #!/usr/bin/python3
 
+from crypto_algos import aes
+from crypto_algos import mac
+
+def decrypt_fn(key, iv):
+    def dec(cipher):
+        return aes.aesDecrypt(cipher, key, 128, mode='cbc', bstr_IV=iv)
+    return dec
 
 if __name__ == '__main__':
-    pass
+    key = b'x' * 16
+    iv = b'y' * 16
+    clear = b'1234567812345678' * 2
+    cipher = aes.aesEncrypt(clear, key, 128, mode='cbc', bstr_IV=iv)
+    cipher = bytearray(mac.sha1MACTagCipher(cipher, clear, key))
+
+    dec_fn = decrypt_fn(key, iv)
+
+    if mac.sha1MACAuthMsg(cipher, key, dec_fn):
+        print('Auth successful')
+    else:
+        print('Auth failed')
+
+    cipher[2] ^= 1
+
+    if mac.sha1MACAuthMsg(cipher, key, dec_fn):
+        print('Auth successful')
+    else:
+        print('Auth failed')

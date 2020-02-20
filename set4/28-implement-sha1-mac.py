61e350d4bbc8caf7832a51ebf5be456289b0b8c8
ullu <ullu@ullu.de>
2020-02-20 22:48:26 +0100

    Remove calls of nonesense functions
    Use only sha1MAC

--- set4/28-implement-sha1-mac.py
+++ set4/28-implement-sha1-mac.py
@@ -12,19 +12,8 @@ if __name__ == '__main__':
     key = b'x' * 16
     iv = b'y' * 16
     clear = b'1234567812345678' * 2
-    cipher = aes.aesEncrypt(clear, key, 128, mode='cbc', bstr_IV=iv)
-    cipher = bytearray(mac.sha1MACTagCipher(cipher, clear, key))
 
-    dec_fn = decrypt_fn(key, iv)
-
-    if mac.sha1MACAuthMsg(cipher, key, dec_fn):
-        print('Auth successful')
-    else:
-        print('Auth failed')
-
-    cipher[2] ^= 1
-
-    if mac.sha1MACAuthMsg(cipher, key, dec_fn):
-        print('Auth successful')
-    else:
-        print('Auth failed')
+    hash1 = mac.sha1MAC(key, clear)
+    hash2 = mac.sha1MAC(key, clear + b'blablaforged')
+    print(hash1)
+    print(hash2)

6f8ce51a67eb0cae3625c06e07d372ffbc9f5de2
ullu <ullu@ullu.de>
2021-03-13 01:33:45 +0100

    Finish SHA1 length extension.
    Fix errors, etc.

--- crypto_algos/attack/hash/sha1.py
+++ crypto_algos/attack/hash/sha1.py
@@ -1,14 +1,29 @@
-def SHA1Padding(bstr: bytes) -> bytes:
-    processed = bstr + b'\x80'
-    padding = b'\x00' * ((56 - (len(processed)) % 64) % 64)
-    msg_bit_len = len(bstr) * 8
-    return processed + padding + struct.pack(b'>Q', msg_bit_len)
+import struct
+from crypto_algos.submodules.sha1.sha1 import Sha1Hash
 
 
-def SHA1RegistersFromHash(bstr_hash: bytes) -> list:
-    regs = []
+def SHA1Padding(bstr: bytes, keylen: int) -> bytes:
+    """
+    Compute and return the SHA1 padding to bstr
+
+    :param bstr:
+    :return:
+    """
+    padding = b"\x00" * ((56 - (keylen + len(bstr + b"\x80")) % 64) % 64)
+    msg_bit_len = (keylen + len(bstr)) * 8
+    return b"\x80" + padding + struct.pack(b">Q", msg_bit_len)
+
+
+def SHA1RegistersFromHash(bstr_hash: bytes) -> tuple:
+    regs = tuple()
     for i in range(5):
-        st = bstr_hash[i*4 : i*4 + 4]
-        regs.append(int(st, base=16))
+        st = bstr_hash[i * 8 : i * 8 + 8]
+        regs += (int(st, base=16),)
     return regs
 
+
+class Sha1LengthExtension(Sha1Hash):
+    def __init__(self, tup, old_msg_len):
+        super().__init__()
+        self._h = tup
+        self._message_byte_length = old_msg_len
--- set4/ch29-sha1-length-extension.py
+++ set4/ch29-sha1-length-extension.py
@@ -5,7 +5,29 @@ from crypto_algos.submodules.sha1.sha1 import sha1
 from crypto_algos.attack.hash import sha1 as attacksha1
 
 if __name__ == "__main__":
-    #print(SHA1Padding(b'a'))
-    h = bytes(sha1(b'a'), 'ascii')
-    print(h)
-    print(attacksha1.SHA1RegistersFromHash(h))
+    # print(SHA1Padding(b'a'))
+    key = b"secret"
+    clear = b"bla"
+    clear = (
+        b"comment1=cooking%20MCs;userdata=foo;comment2=%20like%20a%20pound%20of%20bacon"
+    )
+    # forge = b"a"
+    forge = b";admin=true"
+
+    h = bytes(sha1(key + clear), "ascii")
+
+    regs = attacksha1.SHA1RegistersFromHash(h)
+
+    # ofc normally you don't know the key and you would guess the padding
+    padding = attacksha1.SHA1Padding(clear, len(key))
+    le = attacksha1.Sha1LengthExtension(regs, len(key + clear + padding))
+
+    new_hash = le.update(forge).hexdigest()
+    print("Length extension hash: {}".format(new_hash))
+
+    h = sha1(key + clear + padding + forge)
+    print(
+        "Extension control hash (hash: key + clear + old padding + forge string): {}".format(
+            h
+        )
+    )
